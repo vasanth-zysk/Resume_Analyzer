4f5e833e38b142f92b90d6d00c7549b6e305b58b
VasanthKumar54 <vasnthkumarvasu54@gmail.com>
2025-02-21 12:06:08 +0530

    Made changes to the AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,8 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
-=======
 use Filament\Facades\Filament;
 use Filament\Support\Facades\FilamentAsset;
->>>>>>> 3bd5c97 (Initial Commit)
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,13 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-<<<<<<< HEAD
-        //
-=======
         Filament::registerRenderHook(
             'head.end',
             fn () => '<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>'
         );
->>>>>>> 3bd5c97 (Initial Commit)
     }
 }
