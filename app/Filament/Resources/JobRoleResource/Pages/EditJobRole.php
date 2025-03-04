8d32bd6e40480bd2257c79084a7cca1003f88261
vasanth-zysk <vasanth.p@zysk.tech>
2025-03-04 12:07:53 +0530

    Add redirect URL method to EditJobRole page for improved navigation

--- app/Filament/Resources/JobRoleResource/Pages/EditJobRole.php
+++ app/Filament/Resources/JobRoleResource/Pages/EditJobRole.php
@@ -16,4 +16,9 @@ class EditJobRole extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): ?string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
